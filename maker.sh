6e9e9e372602b2eeb8988d7a34d2e5351b9ba948
sky line <32512284+linesky@users.noreply.github.com>
2024-04-07 01:24:46 +0100

    Add files via upload

--- maker.sh
+++ maker.sh
@@ -1,5 +1,7 @@
 printf "\x1bc\x1b[44;37m"
 roots=$(pwd)/roots
+tmps=/tmp/lists.txt
+tmps2=/tmp/lists2.txt
 mkdir -p $roots
 mkdir -p $roots/usr
 mkdir -p $roots/usr/bin
@@ -8,24 +10,37 @@ mkdir -p $roots/tmp
 mkdir -p $roots/lib
 mkdir -p $roots/dev
 mkdir -p $roots/boot
-cp -p /usr/bin/bash $roots/usr/bin
-cp -p /usr/bin/bash $roots/bin
-cp -p /usr/bin/sh $roots/usr/bin
-cp -p /usr/bin/sh $roots/bin
-cp -p /usr/bin/date $roots/usr/bin
-cp -p /usr/bin/date $roots/bin
-cp -p /usr/bin/nano $roots/usr/bin
-cp -p /usr/bin/nano $roots/bin
-cp -p /usr/bin/grep $roots/usr/bin
-cp -p /usr/bin/grep $roots/usr/bin
-cp -p /usr/bin/grep $roots/bin
-cp -p /usr/bin/sed $roots/usr/bin
-cp -p /usr/bin/sed $roots/bin
+mkdir -p $roots/lib/i386-linux-gnu
+cp  /usr/bin/bash $roots/usr/bin
+cp  /usr/bin/bash $roots/bin
+cp  /usr/bin/sh $roots/usr/bin
+cp  /usr/bin/sh $roots/bin
+cp  /usr/bin/date $roots/usr/bin
+cp  /usr/bin/date $roots/bin
+cp  /usr/bin/nano $roots/usr/bin
+cp  /usr/bin/nano $roots/bin
+cp  /usr/bin/grep $roots/usr/bin
+cp  /usr/bin/grep $roots/usr/bin
+cp  /usr/bin/grep $roots/bin
+cp  /usr/bin/sed $roots/usr/bin
+cp  /usr/bin/sed $roots/bin
 chmod 777 $roots/bin/*
 chmod 777 $roots/usr/bin/*
+printf "" > $tmps
 list1=$(ls $roots/usr/bin/*)
 for l1 in $list1
 do
-printf "'---------------'\n$l1\n"
-ldd "$l1"
+ldd "$l1" | grep  '/lib/' >> $tmps
 done
+awk '{for(i=1;i<=NF;i++) if($i ~ /\/lib\//) print $i}' $tmps > $tmps2
+while IFS= read -r l1
+do
+
+rt="$roots$l1"
+printf "$rt\n"
+cp "$l1" "$rt" 
+done < "$tmps2"
+
+
+
+
